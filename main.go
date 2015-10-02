8937df9a947b539fb7ba9fd3c34b4944a0e2b832
Arjan Schaaf <arjan.schaaf@luminis.eu>
2015-10-02 13:13:44 +0200

    print the header it more expressive

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
         keys = append(keys, k)
     }
     sort.Strings(keys)
-    
-    fmt.Fprintln(w, "<b>RequestURI:</b>", r.RequestURI, "</br>")
-    fmt.Fprintln(w, "<b>RemoveAddr:</b>", r.RemoteAddr, "</br>")
-    fmt.Fprintln(w, "<b>request.TLS:</b>", r.TLS, "</br>")
-    
 
-    
+    fmt.Fprintln(w, "<b>request.RequestURI:</b>", r.RequestURI, "</br>")
+    fmt.Fprintln(w, "<b>request.RemoteAddr:</b>", r.RemoteAddr, "</br>")
+    fmt.Fprintln(w, "<b>request.TLS:</b>", r.TLS, "</br>")
+
+
+
     fmt.Fprintln(w, "<b>Request Headers:</b></br>")
     for _, k := range keys {
         fmt.Fprintln(w, k, ":", r.Header[k], "</br>")
